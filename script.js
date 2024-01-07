c50983d5e6b9c4dd6f9489cedc024e3a33e821f2
Andrei Scafi <andreiscafi@hotmail.com>
2024-01-07 16:58:40 -0300

    Inserting slideshow animation in CSS with Javascript.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const btn = document.querySelector('.banner button');
 const banner = document.querySelector('.banner');
 const slideshowWrapper = document.querySelector('.slideshow-wrapper');
+const slideshow = document.querySelector('.slideshow');
 
 
 btn.addEventListener('click', () => {
     banner.style.display = 'none';
     slideshowWrapper.style.cssText = 'opacity: 1; visibility: visible;'
+    slideshow.style.animation = 'slideshow 24s infinite';
 })
